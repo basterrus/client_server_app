28db952aeab63d2b6dc1ac2d9c2806cf175815aa
Pavel Nedoshivin (@basterrus) <pnedoshivin@gmail.com>
2022-01-28 10:07:50 +0300

    Практическое задание к уроку 6

--- Lesson_6/log/log_config.py
+++ Lesson_6/log/log_config.py
@@ -1,13 +1,20 @@
 import inspect
 import logging
 import os.path
+import sys
+import traceback
 from functools import wraps
+
 # Настройка формата вывода сообщения лога
 formatter = logging.Formatter("%(asctime)s - %(levelname)-8s - %(module)-8s - %(message)s ")
 
 client_logger = logging.getLogger('client')
 server_logger = logging.getLogger('server')
 
+if sys.argv[0].find('client.py') == -1:
+    LOGGER = logging.getLogger('server')
+else:
+    LOGGER = logging.getLogger('client')
 
 # Установка уровня логирования
 level_handler = logging.DEBUG
@@ -24,10 +31,13 @@ def create_dir():
 
 def log(func):
     @wraps(func)
-    def call(*args, **kwargs):
-        outer_func = inspect.stack()[1][3]
-        server_logger.debug(f'Function "{func.__name__}" is called into "{outer_func}"')
-        client_logger.debug(f'Function "{func.__name__}" is called into "{outer_func}"')
-        return func(*args, **kwargs)
+    def log_saver(*args, **kwargs):
+        ret = func(*args, **kwargs)
+        LOGGER.debug(f'Была вызвана функция {func.__name__} c параметрами {args}, {kwargs}. '
+                     f'Вызов из модуля {func.__module__}.'
+                     f'Вызов из функции {traceback.format_stack()[0].strip().split()[-1]}.'
+                     f'Вызов из функции {inspect.stack()[1][3]}')
+        return ret
 
-    return call
+    return log_saver
+
